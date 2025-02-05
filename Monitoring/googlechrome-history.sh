2d3eee30497c5925a49ad09453baad2bfece4dc8
Rüdiger Wolff <rudi.wolff@yahoo.com>
2025-02-05 16:24:55 +0100

    Skript zum Auslesen der Google Chrome History. Inicial Commit.

---
+++ Monitoring/googlechrome-history.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Dieses Skript dient dazu, die Google Chrome History Datei zu kopieren, 
+# so dass sie auch eingesehen werden kann, sollte der User seine History
+# im Browser gelöscht haben.
+
+

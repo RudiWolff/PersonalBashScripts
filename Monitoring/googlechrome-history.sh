c79df67ff8fd54699ff3ee516a8d7fd1434da76b
Rüdiger Wolff <rudi.wolff@yahoo.com>
2025-02-11 17:39:16 +0100

    Vergleich der alten schon weg-kopierten History-Datei mit der neuen History-Datei und evtl. Sicherung der älteren Datei.

--- Monitoring/googlechrome-history.sh
+++ Monitoring/googlechrome-history.sh
@@ -4,4 +4,26 @@
 # so dass sie auch eingesehen werden kann, sollte der User seine History
 # im Browser gelöscht haben.
 
+# Überprüfung, ob root am Start ist:
+if [ $EUID -ne 0 ];then
+    echo >&2 "ERROR: Skript muss als root ausgeführt werden."
+    exit 1
+fi
 
+# Überprüfung, ob b3sum installiert ist:
+which b3sum >& /dev/null
+[ $? -ne 0 ] && apt-get install -y b3sum
+
+# Variablen:
+# Google Chrome History Path:
+gchp="/home/Daniel/.var/app/com.google.Chrome/config/google-chrome/Profile 1/History"
+# Letzte kopierte History-Datei:
+oldHistFile="/home/rwolff/Documentos/log/History/History"
+
+if [ -f $oldHistFile ]; then
+    checksumOldHist=$(b3sum "${oldHistFile}" | cut -d' ' -f1)
+    checksum_gchp=$(b3sum "${gchp}" | cut -d' ' -f1)
+    if [ "$checksumOldHist" != "$checksum_gchp" ]; then
+        cp $oldHistFile $(date +%F'_'%T)_$oldHistFile
+    fi
+fi
